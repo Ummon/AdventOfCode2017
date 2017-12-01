dcf5cc7650f43740add6da0a868ebba5bbce676b
Grégory Burri <gregory.burri@matisa.ch>
2017-12-01 11:34:55 +0100

    Day 1.

---
+++ AdventOfCode2017/Day1.fs
@@ -0,0 +1,8 @@
+﻿module AdventOfCode2017.Day1
+
+let readDigit d = int d - 48
+
+let solveCaptcha (captcha : string) =
+    let numbers = captcha.ToCharArray () |> List.ofArray |> List.map readDigit
+    (List.last numbers :: numbers) |> List.pairwise |> List.map (fun (a, b) -> if a = b then a else 0) |> List.sum
+
