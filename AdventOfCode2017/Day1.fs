1dbe880e8ef13bae1ffce87af575d2d39d0a31c6
Grégory Burri <gregory.burri@matisa.ch>
2017-12-01 12:52:30 +0100

    Code cleaning

--- AdventOfCode2017/Day1.fs
+++ AdventOfCode2017/Day1.fs
@@ -2,11 +2,11 @@
 
 let readDigit d = int d - 48
 
-let solveCaptcha (captcha : string) =
+let solveCaptchaPart1 (captcha : string) =
     let numbers = captcha.ToCharArray () |> List.ofArray |> List.map readDigit
     (List.last numbers :: numbers) |> List.pairwise |> List.map (fun (a, b) -> if a = b then a else 0) |> List.sum
 
-let solveCaptcha' (captcha : string) =
+let solveCaptchaPart2 (captcha : string) =
     let numbers = captcha.ToCharArray () |> Array.map readDigit
     let l = numbers.Length
-    [ for i in [ 0 .. l - 1 ] -> if numbers.[i] = numbers.[(i + l / 2) % l] then numbers.[i] else 0 ] |> List.sum
+    [ for i in 0 .. l - 1 -> if numbers.[i] = numbers.[(i + l / 2) % l] then numbers.[i] else 0 ] |> List.sum
