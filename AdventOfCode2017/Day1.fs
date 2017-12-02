fde56224d8bfbaaa5aff5506cf0626bb9252e173
Greg Burri <greg.burri@gmail.com>
2017-12-02 22:29:04 +0100

    Add day 2

--- AdventOfCode2017/Day1.fs
+++ AdventOfCode2017/Day1.fs
@@ -2,10 +2,11 @@
 
 let readDigit d = int d - int '0'
 
-let solveCaptcha (shift : int) (captcha : string) =
-    let ns = captcha.ToCharArray () |> Array.map readDigit
-    let l = ns.Length
-    [ for i in 0 .. l - 1 -> if ns.[i] = ns.[(i + shift) % l] then ns.[i] else 0 ] |> List.sum
+let parseInput (str : string) : int[] = str.ToCharArray () |> Array.map readDigit
+
+let solveCaptcha (shift : int) (captcha : int[]) =
+    let l = captcha.Length
+    [ for i in 0 .. l - 1 -> if captcha.[i] = captcha.[(i + shift) % l] then captcha.[i] else 0 ] |> List.sum
 
 let solveCaptcha1 = solveCaptcha 1
-let solveCaptcha2 captcha = solveCaptcha (String.length captcha / 2) captcha
+let solveCaptcha2 (captcha : int[]) = solveCaptcha (captcha.Length / 2) captcha
---
+++ AdventOfCode2017/Day2.fs
@@ -0,0 +1,24 @@
+﻿module AdventOfCode2017.Day2
+
+open System
+
+let parseInput (str : string) : int[][] =
+    str.Split ([| '\n' |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun line -> line.Split ([| ' '; '\t' |], StringSplitOptions.RemoveEmptyEntries) |> Array.map int)
+
+let checksum1 (a : int[][]) =
+    a
+    |> Array.map (fun ns -> Array.max ns - Array.min ns)
+    |> Array.sum
+
+let checksum2 (a : int[][]) =
+    a
+    |> Array.map (
+        fun ns ->
+            seq {
+                for a in ns do
+                    for b in ns do
+                        if a <> b && a % b = 0 then yield a / b
+            } |> Seq.head
+    )
+    |> Array.sum
