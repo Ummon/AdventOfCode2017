8f98000da8b4e8a34e95bb27fc60ef04df423a48
Greg Burri <greg.burri@gmail.com>
2017-12-03 15:28:28 +0100

    Day 3

--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -10,11 +10,15 @@ let day2 () =
     let array = File.ReadAllText "Data/day2" |> Day2.parseInput
     sprintf "part1 = %A, part2 = %A" (Day2.checksum1 array) (Day2.checksum2 array)
 
+let day3 () =
+    let input = 325489
+    sprintf "part1 = %A, part2 = %A" (Day3.spiralManhattanDistanceSum input) (Day3.spiralAdjacentSumBiggerThan input)
 
 let doDay (n : int) =
     let result =
         match n with
         | 2 -> day2 ()
+        | 3 -> day3 ()
         | _ -> day1 ()
     printfn "Result of day %i: %s" n result
 
