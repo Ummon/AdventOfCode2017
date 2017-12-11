fa0a890a22d841077628b76c1e6b7cb81520b445
Ummon <greg.burri@gmail.com>
2017-12-11 09:35:31 +0100

    Use coordinates system from https://www.redblobgames.com/grids/hexagons/

--- AdventOfCode2017/Day11.fs
+++ AdventOfCode2017/Day11.fs
@@ -1,21 +1,19 @@
 ﻿module AdventOfCode2017.Day11
 
 let distanceInHex (moves : string) =
-    let distance (x, y) =
-        let x, y = abs x, abs y
-        if y >= x then y + (x - y) / 2 else x
+    let distance (x, y, z) = (abs x + abs y + abs z) / 2
 
     let destination, furthest =
         moves.Split ',' |> Seq.fold (
-            fun ((x, y), furthest) m ->
+            fun ((x, y, z), furthest) m ->
                 let next =
                     match m with
-                    | "n"  -> (x    , y + 2)
-                    | "ne" -> (x + 1, y + 1)
-                    | "se" -> (x + 1, y - 1)
-                    | "s"  -> (x    , y - 2)
-                    | "sw" -> (x - 1, y - 1)
-                    | _    -> (x - 1, y + 1)
+                    | "n"  -> (x    , y + 1, z - 1)
+                    | "ne" -> (x + 1, y    , z - 1)
+                    | "se" -> (x + 1, y - 1, z    )
+                    | "s"  -> (x    , y - 1, z + 1)
+                    | "sw" -> (x - 1, y    , z + 1)
+                    | _    -> (x - 1, y + 1, z    )
                 next, distance next |> max furthest
-            ) ((0, 0), 0)
+            ) ((0, 0, 0), 0)
     distance destination, furthest
