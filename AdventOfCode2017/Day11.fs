ee3af8639cd043fdc9d7c97a16f318c6de46d9b0
Ummon <greg.burri@gmail.com>
2017-12-11 08:52:02 +0100

    "further" -> "furthest"

--- AdventOfCode2017/Day11.fs
+++ AdventOfCode2017/Day11.fs
@@ -7,18 +7,16 @@ let distanceInHex (moves : string list) =
         let x, y = abs x, abs y
         if y >= x then y + (x - y) / 2 else x
 
-    let rec next (x, y) further (moves : string list) =
-        let further' = distance (x, y) |> max further
+    let rec next (x, y) furthest (moves : string list) =
+        let furthest' = distance (x, y) |> max furthest
         match moves with
-        | "n"  :: xs -> next (x, y + 2) further' xs
-        | "ne" :: xs -> next (x + 1, y + 1) further' xs
-        | "se" :: xs -> next (x + 1, y - 1) further' xs
-        | "s"  :: xs -> next (x, y - 2) further' xs
-        | "sw" :: xs -> next (x - 1, y - 1) further' xs
-        | "nw" :: xs -> next (x - 1, y + 1) further' xs
-        | _ -> (x, y), further'
+        | "n"  :: xs -> next (x, y + 2) furthest' xs
+        | "ne" :: xs -> next (x + 1, y + 1) furthest' xs
+        | "se" :: xs -> next (x + 1, y - 1) furthest' xs
+        | "s"  :: xs -> next (x, y - 2) furthest' xs
+        | "sw" :: xs -> next (x - 1, y - 1) furthest' xs
+        | "nw" :: xs -> next (x - 1, y + 1) furthest' xs
+        | _ -> (x, y), furthest'
 
-    let destination, further = next (0, 0) 0 moves
-    distance destination, further
-
-
+    let destination, furthest = next (0, 0) 0 moves
+    distance destination, furthest
